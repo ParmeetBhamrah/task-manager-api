08840c8602ca2bb7a449e12559429b488228409b
Parmeet <parmeetbhamrah@gmail.com>
2026-02-12 23:02:43 +0530

    implement GET /tasks

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
+let tasks = []; // in-memory storage
+let idCounter = 1;
+
 // app.METHOD(PATH, HANDLER) - structure
 app.get("/", (req, res) => { 
-    res.send("Welcome to Task Manager API")
-})
+    res.status(200).json({
+        message:"Welcome to Task Manager API"
+    });
+});
+
+// In REST APIs, URL -> Resource, HTTP Method -> Action
+app.get("/tasks", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: tasks
+    });
+});
